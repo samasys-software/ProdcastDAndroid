9e342d62bb4679602ca59cf79829f46216ca7253
kathirvelnatarajan <30907665+kathirvelnatarajan@users.noreply.github.com>
2017-08-10 21:18:08 -0400

    Applying Changes

--- app/src/main/java/prodcastd/prodcast/samayu/com/prodcastd/LOGIN.java
+++ app/src/main/java/prodcastd/prodcast/samayu/com/prodcastd/LOGIN.java
@@ -1,13 +1,47 @@
 package prodcastd.prodcast.samayu.com.prodcastd;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class LOGIN extends AppCompatActivity {
 
+    EditText userName,password;
+
+    Button signInButton,clearButton;
+    TextView forgotPin,register;
+    boolean cancel = false;
+    View focusView = null;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+         userName = (EditText)findViewById(R.id.logmn);
+        password = (EditText)findViewById(R.id.loginPinNumber);
+        signInButton = (Button)findViewById(R.id.logIn);
+        clearButton = (Button)findViewById(R.id.logClear);
+        forgotPin = (TextView)findViewById(R.id.forgotPin);
+        register =(TextView)findViewById(R.id.register);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userName.setText("");
+                password.setText("");
+            }
+        });
+
     }
 }
